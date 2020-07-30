afacdaea31494265f38e38e361cc9a3e40c756d7
WronskiN <norbertwronski@yahoo.com>
2020-07-30 07:36:58 +0100

    Create updateSearchFilter function for RestaurantMenuList

--- src/views/RestaurantMenuList.js
+++ src/views/RestaurantMenuList.js
@@ -15,279 +15,301 @@ import mealImage from "../assets/restaurant/meal.jpg";
 import BlueIsland from "../components/pages/BlueIsland";
 import Shire from "../components/pages/Shire";
 
+const RestaurantList = [
+  {
+    name: "Blue Island",
+    linkName: "BlueIsland",
+    logoImage: `${Sotonweb}`,
+    address: {
+      city: "Southampton",
+      street: "High Street",
+      houseNumber: 7,
+    },
+    menu: {
+      starters: {
+        meal1: {
+          name: "King prowns in garlic souce",
+          image: `${mealImage}`,
+          ingridients: [
+            "king prowns",
+            "garlic",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "200g",
+          price: "£4.95",
+        },
+        meal2: {
+          name: "Spring rolls x 6 - vegetable",
+          image: `${mealImage}`,
+          ingridients: [
+            "wheat",
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "veg",
+          portion: "200g",
+          price: "£3.99",
+        },
+      },
+      mainCourse: {
+        meal1: {
+          name: "Roast Chicken with potato",
+          image: `${mealImage}`,
+          ingridients: [
+            "chicken",
+            "potato",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "400g",
+          price: "£7.99",
+        },
+        meal2: {
+          name: "Greek Salad",
+          image: `${mealImage}`,
+          ingridients: [
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+            "ice salad",
+          ],
+          type: "veg",
+          portion: "300g",
+          price: "£4.99",
+        },
+      },
+    },
+  },
+  {
+    name: "Shire1",
+    linkName: "Shire",
+    logoImage: `${Sotonweb}`,
+    address: {
+      city: "Southampton",
+      street: "Ols Road",
+      houseNumber: 11,
+    },
+    menu: {
+      starters: {
+        meal1: {
+          name: "King prowns in garlic souce",
+          image: `${mealImage}`,
+          ingridients: [
+            "king prowns",
+            "garlic",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "200g",
+          price: "£4.95",
+        },
+        meal2: {
+          name: "Spring rolls x 6 - vegetable",
+          image: `${mealImage}`,
+          ingridients: [
+            "wheat",
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "veg",
+          portion: "200g",
+          price: "£3.99",
+        },
+      },
+      mainCourse: {
+        meal1: {
+          name: "Roast Chicken with potato",
+          image: `${mealImage}`,
+          ingridients: [
+            "chicken",
+            "potato",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "400g",
+          price: "£7.99",
+        },
+        meal2: {
+          name: "Greek Salad",
+          image: `${mealImage}`,
+          ingridients: [
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+            "ice salad",
+          ],
+          type: "veg",
+          portion: "300g",
+          price: "£4.99",
+        },
+      },
+    },
+  },
+  {
+    name: "Shire",
+    linkName: "Shire",
+    logoImage: `${Sotonweb}`,
+    address: {
+      city: "Southampton",
+      street: "Ols Road",
+      houseNumber: 11,
+    },
+    menu: {
+      starters: {
+        meal1: {
+          name: "King prowns in garlic souce",
+          image: `${mealImage}`,
+          ingridients: [
+            "king prowns",
+            "garlic",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "200g",
+          price: "£4.95",
+        },
+        meal2: {
+          name: "Spring rolls x 6 - vegetable",
+          image: `${mealImage}`,
+          ingridients: [
+            "wheat",
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "veg",
+          portion: "200g",
+          price: "£3.99",
+        },
+      },
+      mainCourse: {
+        meal1: {
+          name: "Roast Chicken with potato",
+          image: `${mealImage}`,
+          ingridients: [
+            "chicken",
+            "potato",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+          ],
+          type: "meat",
+          portion: "400g",
+          price: "£7.99",
+        },
+        meal2: {
+          name: "Greek Salad",
+          image: `${mealImage}`,
+          ingridients: [
+            "garlic",
+            "onions",
+            "paprika",
+            "parsley",
+            "olive oil",
+            "salt",
+            "black papper",
+            "ice salad",
+          ],
+          type: "veg",
+          portion: "300g",
+          price: "£4.99",
+        },
+      },
+    },
+  },
+];
+
 class RestaurantMenuList extends Component {
   state = {
-    restaurantList: "",
-    value: "",
-    restaurant: [
-      {
-        name: "Blue Island",
-        linkName: "BlueIsland",
-        logoImage: `${Sotonweb}`,
-        address: {
-          city: "Southampton",
-          street: "High Street",
-          houseNumber: 7,
-        },
-        menu: {
-          starters: {
-            meal1: {
-              name: "King prowns in garlic souce",
-              image: `${mealImage}`,
-              ingridients: [
-                "king prowns",
-                "garlic",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "200g",
-              price: "£4.95",
-            },
-            meal2: {
-              name: "Spring rolls x 6 - vegetable",
-              image: `${mealImage}`,
-              ingridients: [
-                "wheat",
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "veg",
-              portion: "200g",
-              price: "£3.99",
-            },
-          },
-          mainCourse: {
-            meal1: {
-              name: "Roast Chicken with potato",
-              image: `${mealImage}`,
-              ingridients: [
-                "chicken",
-                "potato",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "400g",
-              price: "£7.99",
-            },
-            meal2: {
-              name: "Greek Salad",
-              image: `${mealImage}`,
-              ingridients: [
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-                "ice salad",
-              ],
-              type: "veg",
-              portion: "300g",
-              price: "£4.99",
-            },
-          },
-        },
-      },
-      {
-        name: "Shire1",
-        linkName: "Shire",
-        logoImage: `${Sotonweb}`,
-        address: {
-          city: "Southampton",
-          street: "Ols Road",
-          houseNumber: 11,
-        },
-        menu: {
-          starters: {
-            meal1: {
-              name: "King prowns in garlic souce",
-              image: `${mealImage}`,
-              ingridients: [
-                "king prowns",
-                "garlic",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "200g",
-              price: "£4.95",
-            },
-            meal2: {
-              name: "Spring rolls x 6 - vegetable",
-              image: `${mealImage}`,
-              ingridients: [
-                "wheat",
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "veg",
-              portion: "200g",
-              price: "£3.99",
-            },
-          },
-          mainCourse: {
-            meal1: {
-              name: "Roast Chicken with potato",
-              image: `${mealImage}`,
-              ingridients: [
-                "chicken",
-                "potato",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "400g",
-              price: "£7.99",
-            },
-            meal2: {
-              name: "Greek Salad",
-              image: `${mealImage}`,
-              ingridients: [
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-                "ice salad",
-              ],
-              type: "veg",
-              portion: "300g",
-              price: "£4.99",
-            },
-          },
-        },
-      },
-      {
-        name: "Shire",
-        linkName: "Shire",
-        logoImage: `${Sotonweb}`,
-        address: {
-          city: "Southampton",
-          street: "Ols Road",
-          houseNumber: 11,
-        },
-        menu: {
-          starters: {
-            meal1: {
-              name: "King prowns in garlic souce",
-              image: `${mealImage}`,
-              ingridients: [
-                "king prowns",
-                "garlic",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "200g",
-              price: "£4.95",
-            },
-            meal2: {
-              name: "Spring rolls x 6 - vegetable",
-              image: `${mealImage}`,
-              ingridients: [
-                "wheat",
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "veg",
-              portion: "200g",
-              price: "£3.99",
-            },
-          },
-          mainCourse: {
-            meal1: {
-              name: "Roast Chicken with potato",
-              image: `${mealImage}`,
-              ingridients: [
-                "chicken",
-                "potato",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-              ],
-              type: "meat",
-              portion: "400g",
-              price: "£7.99",
-            },
-            meal2: {
-              name: "Greek Salad",
-              image: `${mealImage}`,
-              ingridients: [
-                "garlic",
-                "onions",
-                "paprika",
-                "parsley",
-                "olive oil",
-                "salt",
-                "black papper",
-                "ice salad",
-              ],
-              type: "veg",
-              portion: "300g",
-              price: "£4.99",
-            },
-          },
-        },
-      },
-    ],
+    restaurantList: [],
+    search: "",
   };
 
-  handleChange = (e) => {
-    let value = e.target.value.toLowerCase();
+  componentDidMount() {
     this.setState({
-      value: value,
+      restaurantList: [...RestaurantList],
     });
+  }
 
-    const filteredRestaurant = [...this.state.restaurant];
-    console.log(filteredRestaurant);
-    filteredRestaurant.filter((restaurant) =>
-      restaurant.name.toLowerCase() === value.toLowerCase()
-        ? this.setState({
-            restaurant,
-          })
-        : null
+  handleChange = (e) => {
+    this.setState(
+      {
+        search: e.target.value.toLowerCase(),
+      },
+      () => {
+        this.updateSearchFilter();
+      }
     );
   };
 
+  updateSearchFilter = () => {
+    const { search, restaurantList } = this.state;
+    let filteredRestaurants = [];
+    if (search !== "") {
+      filteredRestaurants = restaurantList.filter(
+        (el) => el.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      );
+    } else filteredRestaurants = null;
+
+    if (filteredRestaurants) {
+      this.setState({
+        restaurantList: filteredRestaurants,
+      });
+    } else {
+      this.setState({
+        restaurantList: [...RestaurantList],
+      });
+    }
+    return filteredRestaurants;
+  };
+
   render() {
-    const restaurants = this.state.restaurant;
+    const restaurants = this.state.restaurantList;
     return (
       <MenuPageTemplate>
         <MenuListWrapper>
